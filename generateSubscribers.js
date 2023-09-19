f75ae6093f7cc23276b530044b10822fb46af8d9
Маркова Екатерина <katemark7388@gmail.com>
2023-09-20 00:24:54 +0300

    test

--- generateSubscribers.js
+++ generateSubscribers.js
@@ -13,6 +13,6 @@ async function main() {
       const subscribersString = JSON.stringify(subscribers, null, 2);
       fs.writeFileSync('./src/generated/subscribers.json', subscribersString);
     });
-}
+} 
 
 main();
