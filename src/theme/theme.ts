95386eb368bcb122607e29432c9c29c57bae5426
Маркова Екатерина <katemark7388@gmail.com>
2023-12-16 02:05:30 +0100

    style

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -91,21 +91,6 @@ const customTheme = createTheme({
     }
   },
 
-  input: {
-    "& .MuiFormLabel-root": {
-      fontSize: "16px",
-      fontWeight: "400",
-      color: "#1a1a1c",
-      transform: "translate(8px,6px) scale(1)",
-    },
-    "& .MuiInputBase-root": {
-      padding: "7px!important",
-      borderRadius: "8px", 
-    },
-    "& .MuiInputBase-input": {
-      height: "1.1876em",
-    }
-  },
 
   components: {
     MuiButton: {
@@ -133,6 +118,23 @@ const customTheme = createTheme({
         },
       ],
     },
+    TextField: {
+      style: {
+        "& .MuiFormLabel-root": {
+          fontSize: "16px",
+          fontWeight: "400",
+          color: "#1a1a1c",
+          transform: "translate(8px,6px) scale(1)",
+        },
+        "& .MuiInputBase-root": {
+          padding: "7px!important",
+          borderRadius: "8px", 
+        },
+        "& .MuiInputBase-input": {
+          height: "1.1876em",
+        }
+      }
+    }
   },
 });
 
