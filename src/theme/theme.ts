70c04f372c5ad5e2962565838d048500dacd450b
Маркова Екатерина <katemark7388@gmail.com>
2023-12-16 02:15:45 +0100

    styles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -118,22 +118,25 @@ const customTheme = createTheme({
         },
       ],
     },
-    TextField: {
-      style: {
-        "& .MuiFormLabel-root": {
-          fontSize: "16px",
-          fontWeight: "400",
-          color: "#1a1a1c",
-          transform: "translate(8px,6px) scale(1)",
-        },
-        "& .MuiInputBase-root": {
-          padding: "7px!important",
-          borderRadius: "8px", 
-        },
-        "& .MuiInputBase-input": {
-          height: "1.1876em",
+    MuiInputLabel: {
+      defaultProps: {
+        // props
+      },
+      styleOverrides: {
+        root: {
+            fontSize: "16px",
+            fontWeight: "400",
+            color: "#1a1a1c",
+            transform: "translate(8px,6px) scale(1)",
+            "& .MuiInputBase-root": {
+              padding: "7px!important",
+              borderRadius: "8px", 
+            },
+            "& .MuiInputBase-input": {
+              height: "1.1876em",
+            }
+          }
         }
-      }
     }
   },
 });
