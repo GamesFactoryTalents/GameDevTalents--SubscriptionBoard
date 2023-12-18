9659ba836f73a938ff77dd5551df1d10535fbc35
Маркова Екатерина <katemark7388@gmail.com>
2023-12-18 01:12:29 +0100

    newline test

--- generateSubscribers.js
+++ generateSubscribers.js
@@ -7,9 +7,11 @@ async function main() {
   fs.createReadStream('./src/generated/vacancies.csv')
     .pipe(csv())
     .on('data', (row) => {
+      console.log(row.jobDescription.contains(/\r\n/g));
       row.jobDescription.replace(/\r\n/g,'<br/>');
       row.requirements.replace(/\r\n/g,'<br/>');
       row.responsibilities.replace(/\r\n/g,'<br/>');
+      console.log(row);
       subscribers.push(row);
     })
     .on('end', () => {
