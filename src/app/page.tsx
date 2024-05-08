64011ddcbf34d1ced5b3e490c742be7bfae3e9e8
Маркова Екатерина <katemark7388@gmail.com>
2024-05-08 18:21:41 +0200

    background fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import { Container } from "@mui/material";
 import FilterMenu from "../components/FilterMenu/FilterMenu";
 import SubscriptionItemList from "../components/SubscriptionItemList";
 import SubscriptionsPagination from "../components/SubscriptionsPagination/SubscriptionsPagination";
-import background from "../background.jpg";
 
 export default function Home() {
   return (
-    <Container sx={{ p: 2, backgroundImage: `url(${background})`,backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundAttachment: "fixed", maxWidth: "100%!important" }}>
+    <Container sx={{ p: 2, backgroundImage: `url("/background.jpg")`,backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundAttachment: "fixed", maxWidth: "100%!important" }}>
       <FilterMenu />
       <SubscriptionsPagination />
     </Container>
