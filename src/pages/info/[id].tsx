2c48e621c1892bb2803b93fe084831d39a0de99a
Oleg Paliy <75069725+GFTalents@users.noreply.github.com>
2025-05-28 08:48:14 +0300

    text upate to Careers Zone

--- src/pages/info/[id].tsx
+++ src/pages/info/[id].tsx
@@ -391,7 +391,7 @@ const InfoPage = ({ id, info }: Props) => {
             color="primary"
             style={{borderRadius: "10px",width: "200px", backgroundColor: "#3f51b5",}}
           >
-            Join Career Area to Connect with Recruiters Directly
+            Join Careers Zone to Connect with Recruiters Directly
           </Button>
           <Link
             href="/"
